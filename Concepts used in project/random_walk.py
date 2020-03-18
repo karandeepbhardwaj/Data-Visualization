483146def3335ed5646b1a6b94bfd2549236772f
Karandeep <karandiip@gmail.com>
2020-03-18 15:45:02 -0400

    Added concepts related to scatter and plot method in pyplot

---
+++ Concepts used in project/random_walk.py
@@ -0,0 +1,39 @@
+from random import choice
+
+
+def get_step():
+    direction = choice([1, -1])
+    distance = choice([0, 1, 2, 3, 4])
+    return direction * distance
+
+
+class RandomWalk():
+    """A Class to generate random walks."""
+
+    def __init__(self, num_points=5000):
+        """Initialize the attributes of a walk"""
+        self.num_points = num_points
+
+        # All walk start at (0,0).
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        """Calculate all the points in the walk."""
+        # Keep taking steps until the walk reaches the desired length.
+        while len(self.x_values) < self.num_points:
+
+            # Decide which direction to go and how far to go in that direction
+            x_step = get_step()
+            y_step = get_step()
+
+            # Reject moves that go no where.
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Calculate the next x and y values.
+            next_x = self.x_values[-1] + x_step
+            next_y = self.y_values[-1] + y_step
+            self.x_values.append(next_x)
+            self.y_values.append(next_y)
+
---
+++ Concepts used in project/rw_visual.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+
+while True:
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+
+    # Set the size of the plotting window.
+    plt.figure(figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # plt.plot(rw.x_values, rw.y_values, linewidth=5)
+
+    # Remove the axes.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n):\n")
+    if keep_running == 'n':
+        break
