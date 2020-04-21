097bfe4694abee05282fa919f7c5093965338f66
Karandeep <karandiip@gmail.com>
2020-04-21 13:47:07 -0400

    Added last changes

---
+++ Data Downloading/highs_lows.py
@@ -0,0 +1,9 @@
+import csv
+
+file_name = 'sitka_weather_07-2014.csv'
+with open(file_name) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
